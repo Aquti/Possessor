9714e9cedf0bf97065dd6a6a098a77ba2a8761b4
Aquti <103832857+Aquti@users.noreply.github.com>
2025-01-11 17:26:23 +0100

    Update Key.lua

--- Key.lua
+++ Key.lua
@@ -1,1 +1,11 @@
-local key = "1"
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local key = ReplicatedStorage:FindFirstChild("Key")
+
+if not key then
+    key = Instance.new("StringValue")
+    key.Name = "Key"
+    key.Parent = ReplicatedStorage
+end
+
+key.value = "	4;	7"
